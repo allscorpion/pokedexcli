4ecf6357f4ba46fc4e1c7541cfc23eef07c6c55c
allscorpion <deadlyryz@hotmail.co.uk>
2025-10-17 21:30:37 +0100

    clean up duplicated api code

--- internal/pokedex/pokedex.go
+++ internal/pokedex/pokedex.go
@@ -10,7 +10,7 @@ import (
 
 
 
-func ConvertBytesToJson[T any](data []byte) (T, error) {
+func convertBytesToJson[T any](data []byte) (T, error) {
     var obj T
     if err := json.Unmarshal(data, &obj); err != nil {
         return *new(T), err
@@ -18,146 +18,61 @@ func ConvertBytesToJson[T any](data []byte) (T, error) {
     return obj, nil
 }
 
-func GetLocationAreas(config *c.Config, url string) (c.LocationAreas, error) {
+func getApiRequest[T any](config *c.Config, url string) (T, error) {
+	var zero T
+
 	cacheData, exists := config.Cache.Get(url)
 	if exists {
-		locationArea, err := ConvertBytesToJson[c.LocationAreas](cacheData)
+		parsedCacheData, err := convertBytesToJson[T](cacheData)
 		
 		if err != nil {
-			return c.LocationAreas{}, err
+			return zero, err
 		}
 
-
-		return locationArea, nil
+		return parsedCacheData, nil
 	}
 
-
-	req, err := http.NewRequest("GET", url, nil);
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return c.LocationAreas{}, err
+		return zero, err
 	}
 
 	client := &http.Client{}
-	res, err := client.Do(req);
+	res, err := client.Do(req)
 	if err != nil {
-		return c.LocationAreas{}, err;
+		return zero, err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode > 299 {
-		return c.LocationAreas{}, fmt.Errorf("response failed with status code %v", res.StatusCode)
+		return zero, fmt.Errorf("response failed with status code %v", res.StatusCode)
 	}
 
-	var la c.LocationAreas;
-	decoder := json.NewDecoder(res.Body);
-	if err := decoder.Decode(&la); err != nil {
-		return c.LocationAreas{}, err;
+	var obj T
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&obj); err != nil {
+		return zero, err
 	}
 
-	data, err := json.Marshal(la)
+	data, err := json.Marshal(obj)
 	if err != nil {
-		return c.LocationAreas{}, err
+		return zero, err
 	}
 
 	config.Cache.Add(url, data);
 
-	return la, nil;
+	return obj, nil
 }
 
-
+func GetLocationAreas(config *c.Config, url string) (c.LocationAreas, error) {
+	return getApiRequest[c.LocationAreas](config, url)
+}
 
 func GetLocationArea(config *c.Config, url string) (c.LocationArea, error) {
-	cacheData, exists := config.Cache.Get(url)
-	if exists {
-		locationArea, err := ConvertBytesToJson[c.LocationArea](cacheData)
-		
-		if err != nil {
-			return c.LocationArea{}, err
-		}
-
-
-		return locationArea, nil
-	}
-
-
-	req, err := http.NewRequest("GET", url, nil);
-	if err != nil {
-		return c.LocationArea{}, err
-	}
-
-	client := &http.Client{}
-	res, err := client.Do(req);
-	if err != nil {
-		return c.LocationArea{}, err;
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode > 299 {
-		return c.LocationArea{}, fmt.Errorf("response failed with status code %v", res.StatusCode)
-	}
-
-	var la c.LocationArea;
-	decoder := json.NewDecoder(res.Body);
-	if err := decoder.Decode(&la); err != nil {
-		return c.LocationArea{}, err;
-	}
-
-	data, err := json.Marshal(la)
-	if err != nil {
-		return c.LocationArea{}, err
-	}
-
-	config.Cache.Add(url, data);
-
-	return la, nil;
+	return getApiRequest[c.LocationArea](config, url)
 }
-
-
 
 func GetPokemonByName(config *c.Config, name string) (c.Pokemon, error) {
 	url := "https://pokeapi.co/api/v2/pokemon/" + name;
-
-	cacheData, exists := config.Cache.Get(url)
-	if exists {
-		pokemon, err := ConvertBytesToJson[c.Pokemon](cacheData)
-		
-		if err != nil {
-			return c.Pokemon{}, err
-		}
-
-
-		return pokemon, nil
-	}
-
-
-	req, err := http.NewRequest("GET", url, nil);
-	if err != nil {
-		return c.Pokemon{}, err
-	}
-
-	client := &http.Client{}
-	res, err := client.Do(req);
-	if err != nil {
-		return c.Pokemon{}, err;
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode > 299 {
-		return c.Pokemon{}, fmt.Errorf("response failed with status code %v", res.StatusCode)
-	}
-
-	var la c.Pokemon;
-	decoder := json.NewDecoder(res.Body);
-	if err := decoder.Decode(&la); err != nil {
-		return c.Pokemon{}, err;
-	}
-
-	data, err := json.Marshal(la)
-	if err != nil {
-		return c.Pokemon{}, err
-	}
-
-	config.Cache.Add(url, data);
-
-	return la, nil;
+	return getApiRequest[c.Pokemon](config, url)
 }
